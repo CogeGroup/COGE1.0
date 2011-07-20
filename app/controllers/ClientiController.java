9b77dfb455aecd9c676abba9432616c3a3372fc1
Solving Team <l.gismant@solvingteam.it>
2011-07-20 14:07:32 +0200

    startup

---
+++ app/controllers/ClientiController.java
@@ -0,0 +1,17 @@
+package controllers;
+
+import play.*;
+import play.modules.paginate.ValuePaginator;
+import play.mvc.*;
+
+import java.util.*;
+
+import models.*;
+
+public class ClientiController extends Controller {
+
+    public static void index() {
+        render();
+    }
+
+}
---
+++ app/controllers/RisorseController.java
@@ -0,0 +1,17 @@
+package controllers;
+
+import play.*;
+import play.modules.paginate.ValuePaginator;
+import play.mvc.*;
+
+import java.util.*;
+
+import models.*;
+
+public class RisorseController extends Controller {
+
+    public static void index() {
+        render();
+    }
+
+}
