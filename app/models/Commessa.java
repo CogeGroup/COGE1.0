ca516b6242ad649e1891e10c6155242d9fe719c2
Solving Team <l.gismant@solvingteam.it>
2011-09-05 14:50:24 +0200

    configuration

--- app/models/Commessa.java
+++ app/models/Commessa.java
@@ -177,9 +177,9 @@ public class Commessa extends GenericModel{
 		List<Tariffa> listaTariffe = query.fetch();
 		if (listaTariffe != null && !listaTariffe.isEmpty()){
 		   for(Tariffa t:listaTariffe){
-			   if(!(t.commessa instanceof CommessaACorpo)){
+			   //if(!(t.commessa instanceof CommessaACorpo)){
 				   listaCommesse.add(t.commessa);
-			   }
+			   //}
 		   }
 		}
 		return listaCommesse;
