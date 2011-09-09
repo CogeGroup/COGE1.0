cc362509c004ae9f64e233105073b364b64e551f
Solving Team <l.gismant@solvingteam.it>
2011-09-09 14:54:27 +0200

    configurazioni

--- app/secure/SecureCOGE.java
+++ app/secure/SecureCOGE.java
@@ -17,13 +17,11 @@ public class SecureCOGE extends Secure {
 
     @Before(unless={"login", "authenticate", "logout"})
     static void checkAccess() throws Throwable {
-    	System.out.println("SECURE COGE");
     	
     	//System.out.println("-------DENTRO SecureCOGE " + Application.request.controller + "/" + Application.request.actionMethod );
         // Authent
         if(!session.contains("username")) {
             flash.put("url", "GET".equals(request.method) ? request.url : "/"); // seems a good default
-            System.out.println(flash.get("url"));
             login();
         }
         else{
