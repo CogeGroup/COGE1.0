65d58f9f142bc5cbe954a7e6c31fc06634da2222
Alessio Canestrelli <a.canestrelli@solvingteam.it>
2011-11-15 10:54:41 +0100

    coge mobile

---
+++ app/controllers/CogeMobile.java
@@ -0,0 +1,18 @@
+package controllers;
+
+import models.Utente;
+import play.db.jpa.GenericModel.JPAQuery;
+import play.mvc.*;
+
+public class CogeMobile extends Controller {
+
+    public static void index() {
+    	render();
+    }
+    
+    public static void auth(String username, String psw) {
+    	System.out.println("username: " + username);
+    	System.out.println("psw: " + psw);
+    }
+
+}
