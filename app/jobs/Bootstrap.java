5d3d7e71b68791ff6ae629960a00df3b1f1d0963
Alessio Canestrelli <a.canestrelli@solvingteam.it>
2012-09-05 15:10:47 +0200

    database, bootstrap, images per geekLogica

--- app/jobs/Bootstrap.java
+++ app/jobs/Bootstrap.java
@@ -3,13 +3,22 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import org.joda.time.DateTime;
-
-import models.*;
-
-import play.jobs.*;
+import models.Cliente;
+import models.Commessa;
+import models.CommessaACorpo;
+import models.Costo;
+import models.DettaglioRapportoAttivita;
 import models.Job;
-import play.test.Fixtures;
+import models.RapportoAttivita;
+import models.RapportoLavoro;
+import models.Risorsa;
+import models.Ruolo;
+import models.Tariffa;
+import models.TipoRapportoLavoro;
+import models.TipoStatoRisorsa;
+import models.Utente;
+import play.jobs.OnApplicationStart;
+import utility.EncryptionUtility;
 	 
 @OnApplicationStart
 public class Bootstrap extends play.jobs.Job {
@@ -30,6 +39,7 @@ public class Bootstrap extends play.jobs.Job {
 //      System.out.println(".....init"); 
 //      try {
 //	   setup();
+//    	  setupGeek();
 //	} catch (ParseException e1) {
 //		e1.printStackTrace();
 //	}
@@ -159,8 +169,8 @@ public class Bootstrap extends play.jobs.Job {
  
       public void setup() throws ParseException{
     	  
-      Risorsa r1 = new Risorsa("a","pippo", "pippo", new SimpleDateFormat("dd/MM/yyyy").parse("01/06/2011"));
-      Risorsa r2 = new Risorsa("b","risorsa2", "risorsa2",new SimpleDateFormat("dd/MM/yyyy").parse("01/06/2011"));
+      Risorsa r1 = new Risorsa("admin","admin", "admin", new SimpleDateFormat("dd/MM/yyyy").parse("01/06/2011"));
+      Risorsa r2 = new Risorsa("segreteria","segreteria", "segreteria",new SimpleDateFormat("dd/MM/yyyy").parse("01/06/2011"));
       
       TipoRapportoLavoro tipoRappLav1 = new TipoRapportoLavoro("tipoRap1","cod");
       TipoRapportoLavoro tipoRappLav2 = new TipoRapportoLavoro("tipoRap2","cod2");
@@ -217,8 +227,8 @@ public class Bootstrap extends play.jobs.Job {
       float importoMensile = 2000;
       Costo cs1 = new Costo(importoGiornaliero,importoMensile,new Date(),r1);
       cs1.save();
-      
-      Ruolo ruolo1 = new Ruolo("admin").save();
+
+    	  Ruolo ruolo1 = new Ruolo("admin").save();
       Ruolo ruolo2 = new Ruolo("segreteria").save();
       
       
@@ -260,7 +270,7 @@ public class Bootstrap extends play.jobs.Job {
       j26.addRuolo(ruolo1);
       j26.save();
       
-      Utente u1 = new Utente("a","a",true);
+      Utente u1 = new Utente("admin",EncryptionUtility.encrypt("admin"),true);
       
       u1.addRuolo(ruolo1);
       
@@ -328,12 +338,7 @@ public class Bootstrap extends play.jobs.Job {
       j25.addRuolo(ruolo2);
       j25.save();
       
-      
-      
-      
-     
-      
-      Utente u2 = new Utente("s","s",true);
+      Utente u2 = new Utente("segreteria",EncryptionUtility.encrypt("segreteria"),true);
       u2.addRuolo(ruolo2);
       
       u2.risorsa=r2;
@@ -348,7 +353,145 @@ public class Bootstrap extends play.jobs.Job {
     
       }   
       
+      public void setupGeek() throws ParseException{
+    	  TipoStatoRisorsa tipoStatRis = new TipoStatoRisorsa("IN","ingresso").save();
+    	  tipoStatRis = new TipoStatoRisorsa("Uscita","OUT").save();
+    	  tipoStatRis = new TipoStatoRisorsa("Trasferminto Rapporto","TRAS").save();
+    	  tipoStatRis = new TipoStatoRisorsa("Sospeso","SOSP").save();
+    	  tipoStatRis = new TipoStatoRisorsa("Chiuso","CHIUSO").save();
+    	  tipoStatRis = new TipoStatoRisorsa("Attivo","ATTIVO").save();
+        
+    	Risorsa r1 = new Risorsa("admin","admin", "admin", new SimpleDateFormat("dd/MM/yyyy").parse("01/06/2011"));
+    	r1.tipoStatoRisorsa = tipoStatRis;
+    	r1.save();
+        Risorsa r2 = new Risorsa("segreteria","segreteria", "segreteria",new SimpleDateFormat("dd/MM/yyyy").parse("01/06/2011"));
+        r2.tipoStatoRisorsa = tipoStatRis;
+        r2.save();
+        
+        Ruolo ruolo1 = new Ruolo("admin").save();
+        Ruolo ruolo2 = new Ruolo("segreteria").save();
+        
+        
+        //job da assegnare a tutti gli utenti
+        Job j0 = new Job("LogInController/*").save();
+        //jobs del ruolo admin
+        Job j1 = new Job("AccountController/*");
+        Job j2 = new Job("ClientiController/*");
+        Job j3 = new Job("CommesseController/*");
+        Job j4 = new Job("CostiController/*").save();
+        Job j5 = new Job("RendicontoAttivitaController/*");
+        Job j6 = new Job("RisorseController/*");
+        Job j7 = new Job("TariffeController/*");
+        Job j8 = new Job("RapportoLavoroController/*");
+        Job j9 = new Job("TipoRapportoLavoroController/*");
+        Job j26 = new Job("CostoCommessaController/*");
+        
+        j0.addRuolo(ruolo1);
+        j0.addRuolo(ruolo2);
+        j0.save();
+        j1.addRuolo(ruolo1);
+        j1.save();
+        j2.addRuolo(ruolo1);
+        j2.save();
+        j3.addRuolo(ruolo1);
+        j3.save();
+        j4.addRuolo(ruolo1);
+        j4.save();
+        j5.addRuolo(ruolo1);
+        j5.save();
+        j6.addRuolo(ruolo1);
+        j6.save();
+        j7.addRuolo(ruolo1);
+        j7.save();
+        j8.addRuolo(ruolo1);
+        j8.save();
+        j9.addRuolo(ruolo1);
+        j9.save();
+        j26.addRuolo(ruolo1);
+        j26.save();
+        
+        Utente u1 = new Utente("admin",EncryptionUtility.encrypt("admin"),true);
+        
+        u1.addRuolo(ruolo1);
+        
+        u1.risorsa=r1;
+        u1.save();
+        
+//        Ruolo r = Ruolo.findById(1);
+//        r.remove();
+//        r.save();
+//        
+        
+        
+        
       
+        //jobs associati al ruolo segreteria
+        Job j10 = new Job("AccountController/index");
+        Job j11 = new Job("AccountController/listUtenti");
+        Job j12 = new Job("ClientiController/list");
+        Job j13 = new Job("ClientiController/show");
+        Job j14 = new Job("CommesseController/list");
+        Job j15 = new Job("CommesseController/show");
+        Job j16 = new Job("RendicontoAttivitaController/*");
+        Job j17 = new Job("RisorseController/list");
+        Job j18 = new Job("RisorseController/index");
+        Job j19 = new Job("RisorseController/show");
+        Job j20 = new Job("RisorseController/listRapportoLavoro");
+        Job j21 = new Job("TariffeController/list");
+        Job j22 = new Job("RapportoLavoroController/list");
+        Job j23 = new Job("RapportoLavoroController/show");
+        Job j24 = new Job("CostiController/list");
+        Job j25 = new Job("TipoRapportoLavoroController/index");
+        
+        
+       
+        j10.addRuolo(ruolo2);
+        j10.save();
+        j11.addRuolo(ruolo2);
+        j11.save();
+        j12.addRuolo(ruolo2);
+        j12.save();
+        j13.addRuolo(ruolo2);
+        j13.save();
+        j14.addRuolo(ruolo2);
+        j14.save();
+        j15.addRuolo(ruolo2);
+        j15.save();
+        j16.addRuolo(ruolo2);
+        j16.save();
+        j17.addRuolo(ruolo2);
+        j17.save();
+        j18.addRuolo(ruolo2);
+        j18.save();
+        j19.addRuolo(ruolo2);
+        j19.save();
+        j20.addRuolo(ruolo2);
+        j20.save();
+        j21.addRuolo(ruolo2);
+        j21.save();
+        j22.addRuolo(ruolo2);
+        j22.save();
+        j23.addRuolo(ruolo2);
+        j23.save();
+        j24.addRuolo(ruolo2);
+        j24.save();
+        j25.addRuolo(ruolo2);
+        j25.save();
+        
+        Utente u2 = new Utente("segreteria",EncryptionUtility.encrypt("segreteria"),true);
+        u2.addRuolo(ruolo2);
+        
+        u2.risorsa=r2;
+        u2.save();
+        
+//        Tariffa t1 = new Tariffa(new SimpleDateFormat("dd/MM/yyyy").parse("01/07/2011"),100,r2,cm1);
+//        t1.dataFine = new SimpleDateFormat("dd/MM/yyyy").parse("31/08/2011");
+//        Tariffa t2 = new Tariffa(new SimpleDateFormat("dd/MM/yyyy").parse("01/09/2011"),200,r2,cm1);
+//        t1.save();
+//        t2.save();
+        
+      
+        }   
       
  
     
